c959e61bc9df2d37d9fc345c844e4204cc619fa7
IchHabeKeinGrip <90575828+IchHabeKeinGrip@users.noreply.github.com>
2021-09-26 15:41:19 +0200

    Main Files for Calculator!
    ""

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.btMinus = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btPlus = new System.Windows.Forms.Button();
+            this.btGeteilt = new System.Windows.Forms.Button();
+            this.btMal = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.Location = new System.Drawing.Point(0, 319);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(117, 107);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            // 
+            // bt7
+            // 
+            this.bt7.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7.Location = new System.Drawing.Point(0, 93);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(117, 107);
+            this.bt7.TabIndex = 1;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            // 
+            // bt6
+            // 
+            this.bt6.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt6.Location = new System.Drawing.Point(246, 206);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(117, 107);
+            this.bt6.TabIndex = 2;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            // 
+            // bt5
+            // 
+            this.bt5.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt5.Location = new System.Drawing.Point(123, 206);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(117, 107);
+            this.bt5.TabIndex = 3;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            // 
+            // bt8
+            // 
+            this.bt8.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt8.Location = new System.Drawing.Point(123, 93);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(117, 107);
+            this.bt8.TabIndex = 4;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            // 
+            // bt4
+            // 
+            this.bt4.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt4.Location = new System.Drawing.Point(0, 206);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(117, 107);
+            this.bt4.TabIndex = 5;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            // 
+            // bt3
+            // 
+            this.bt3.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt3.Location = new System.Drawing.Point(246, 319);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(117, 107);
+            this.bt3.TabIndex = 6;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            // 
+            // bt2
+            // 
+            this.bt2.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt2.Location = new System.Drawing.Point(123, 319);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(117, 107);
+            this.bt2.TabIndex = 7;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            // 
+            // bt9
+            // 
+            this.bt9.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt9.Location = new System.Drawing.Point(246, 93);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(117, 107);
+            this.bt9.TabIndex = 8;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            // 
+            // btMinus
+            // 
+            this.btMinus.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMinus.Location = new System.Drawing.Point(246, 432);
+            this.btMinus.Name = "btMinus";
+            this.btMinus.Size = new System.Drawing.Size(117, 107);
+            this.btMinus.TabIndex = 9;
+            this.btMinus.Text = "-";
+            this.btMinus.UseVisualStyleBackColor = true;
+            // 
+            // bt0
+            // 
+            this.bt0.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt0.Location = new System.Drawing.Point(124, 432);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(117, 107);
+            this.bt0.TabIndex = 10;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // btPlus
+            // 
+            this.btPlus.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPlus.Location = new System.Drawing.Point(0, 432);
+            this.btPlus.Name = "btPlus";
+            this.btPlus.Size = new System.Drawing.Size(117, 107);
+            this.btPlus.TabIndex = 11;
+            this.btPlus.Text = "+";
+            this.btPlus.UseVisualStyleBackColor = true;
+            // 
+            // btGeteilt
+            // 
+            this.btGeteilt.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGeteilt.Location = new System.Drawing.Point(246, 542);
+            this.btGeteilt.Name = "btGeteilt";
+            this.btGeteilt.Size = new System.Drawing.Size(117, 107);
+            this.btGeteilt.TabIndex = 12;
+            this.btGeteilt.Text = "/";
+            this.btGeteilt.UseVisualStyleBackColor = true;
+            // 
+            // btMal
+            // 
+            this.btMal.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMal.Location = new System.Drawing.Point(0, 542);
+            this.btMal.Name = "btMal";
+            this.btMal.Size = new System.Drawing.Size(117, 107);
+            this.btMal.TabIndex = 13;
+            this.btMal.Text = "*";
+            this.btMal.UseVisualStyleBackColor = true;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(117, 24);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(0, 39);
+            this.lb1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 661);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.btMal);
+            this.Controls.Add(this.btGeteilt);
+            this.Controls.Add(this.btPlus);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.btMinus);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form1";
+            this.Text = "Calculator v01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button btMinus;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button btPlus;
+        private System.Windows.Forms.Button btGeteilt;
+        private System.Windows.Forms.Button btMal;
+        private System.Windows.Forms.Label lb1;
+    }
+}
+
